e2f71c86d7b13422a541005d23b23720c9835dc2
Tomasz Kamiński <tomaszkam@gmail.com>
2015-07-17 16:31:25 +0200

    C10. The BOM mark used in localisation files is not handled correctly
    Added fixup.sh script that remove character created generated
    by incorrect BOM handling fixing the localisation problems.

---
+++ bin/fixup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+  echo "Invalid number of arguments" >&2;
+  echo "Usage: fixups.sh moddirectory" >&2;
+  exit 1;
+fi
+
+MOD_DIR=$1
+
+LOCALISATIONS="${MOD_DIR}/history/countries"
+for LOCALISATION_FILE in "${LOCALISATIONS}"/*; do
+  sed -ie 's/^^ï»¿//g' "$LOCALISATION_FILE"
+done 
