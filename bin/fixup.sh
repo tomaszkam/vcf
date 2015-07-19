68fad21ba709a52fec55e5a412cc4fc794608b08
Tomasz Kamiński <tomaszkam@gmail.com>
2015-07-19 18:12:41 +0200

    C1,C2,C4,C8,C9. Added custom culture localisation for other languages.
    Added spanish,german and french version of the converted_custom_cultures
    file. Fixes following problems for all supported languages
    C1. Israelite culture group show as "israelite" in the Culture selection
        menu.
    C2. Old Saxon culture is localized as "Saxon", causing two cultures
        named saxon in "Germanic" group.
    C4. Reformed Zunist heresy (DARKNESS) and Jain heresy (TERAPANTHA) is
        missing localization in converted_heresies_l_lang.yml.
    C8. Jewish heresies and Zoroastrian heresies list their parent religion
        as heresy, but lack a localization for it.
    C9. Anglo-Saxon is localized as "Saxon", causing two cultures named
        saxon to exists.

--- bin/fixup.sh
+++ bin/fixup.sh
@@ -8,7 +8,7 @@ fi
 
 MOD_DIR=$1
 
-LOCALISATIONS="${MOD_DIR}/history/countries"
+LOCALISATIONS="${MOD_DIR}/localisation"
 for LOCALISATION_FILE in "${LOCALISATIONS}"/*; do
   sed -ie 's/^^ï»¿//g' "$LOCALISATION_FILE"
 done 
